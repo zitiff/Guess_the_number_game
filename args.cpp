a03e307b0820740e704ea17385419a4526e593df
@mochalova <mochalova.oi@nppgamma.ru>
2023-05-01 17:21:22 +0300

    add parameter "level"

--- args.cpp
+++ args.cpp
@@ -19,5 +19,23 @@ int argsChecking(int argc, char** argv, int& max_value)
 			io("", 0);
 			return -1;
 		}
+		else if (arg1_value == "-level")
+		{
+			if (argc < 3) {
+				cout << "Wrong usage! The argument '-max' requires some value!" << std::endl;
+				return -1;
+			}
+			int level = stoi(argv[2]);
+			if (level == 1)
+				max_value = 10;
+			else if (level == 2)
+				max_value = 50;
+			else if (level == 3)
+				max_value = 100;
+			else
+				cout << "Incorrect value of argument '-level'" << std::endl;
+			
+			return 0;
+		}
 	}
 }
